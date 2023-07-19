659ee923952b8ed6364064929fe599d71ee6c639
wxwilliamc <chongwx-pm18@student.tarc.edu.my>
2023-07-19 20:58:46 +0800

    Usermenu - Logged In

--- app/components/navbar/usermenu.tsx
+++ app/components/navbar/usermenu.tsx
@@ -8,8 +8,18 @@ import {Avatar, MenuItem} from '..';
 // Hooks
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
+// Next Auth
+import { signOut } from 'next-auth/react';
+// Types
+import { SafeUser } from '@/app/types';
 
-const UserMenu = () => {
+interface usermenuProps {
+    currentUser?: SafeUser | null
+}
+
+const UserMenu:React.FC<usermenuProps> = ({
+    currentUser
+}) => {
     const registerModal = useRegisterModal();
     const loginModal = useLoginModal();
     const [isOpen, setIsOpen] = useState(false);
@@ -79,16 +89,46 @@ const UserMenu = () => {
                         flex-col
                         cursor-pointer
                     '>
-                       <>
-                        <MenuItem 
-                            onClick={loginModal.onOpen}
-                            label='Login'
-                        />
-                        <MenuItem 
-                            onClick={registerModal.onOpen}
-                            label='Sign Up'
-                        />
+                    {currentUser ? (
+                        <>
+                            <MenuItem 
+                                onClick={() => {}}
+                                label='My trips'
+                            />
+                            <MenuItem 
+                                onClick={() => {}}
+                                label='My favorites'
+                            />
+                            <MenuItem 
+                                onClick={() => {}}
+                                label='My reservations'
+                            />
+                            <MenuItem 
+                                onClick={() => {}}
+                                label='My properties'
+                            />
+                            <MenuItem 
+                                onClick={() => {}}
+                                label='Airbnb my home'
+                            />
+                            <hr />
+                            <MenuItem 
+                                onClick={() => signOut()}
+                                label='Logout'
+                            />
+                        </> 
+                    ): (
+                        <>
+                            <MenuItem 
+                                onClick={loginModal.onOpen}
+                                label='Login'
+                            />
+                            <MenuItem 
+                                onClick={registerModal.onOpen}
+                                label='Sign Up'
+                            />
                        </> 
+                    )}
                     </div>
                 </div>
             )}
