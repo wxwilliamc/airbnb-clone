a71cd8d68ed064ee1a9f02437f9a11309c5b53ee
wxwilliamc <chongwx-pm18@student.tarc.edu.my>
2023-07-29 20:59:49 +0800

    Dynamic Search / Error / Middleware

---
+++ app/error.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect } from "react";
+import { EmptyState } from "./components";
+
+interface ErrorProps {
+  error: Error
+}
+
+const Error:React.FC<ErrorProps> = ({
+  error
+}) => {
+
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+
+  return (
+    <div>
+      <EmptyState 
+        title="Uh Oh"
+        subtitle="Something went wrong!"
+      />
+    </div>
+  )
+}
+
+export default Error
